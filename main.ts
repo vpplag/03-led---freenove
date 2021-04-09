82f8843b6c30dea2ba576aded1315a8483ec9ca4
vpplag <74141971+vpplag@users.noreply.github.com>
2021-04-09 17:42:19 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+})
+input.onButtonPressed(Button.B, function () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    for (let index = 0; index < 3; index++) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        basic.pause(100)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        basic.pause(100)
+    }
+})
